0808c36cc018be60c01d05bcd13e44dd093c4c84
Ridho Purnomo <72849578+ridopurnomo86@users.noreply.github.com>
2023-12-12 10:11:54 +0700

    add:create proxy middleware

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const morgan = require("morgan");
+const {
+  createProxyMiddleware,
+  responseInterceptor,
+} = require("http-proxy-middleware");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+  })
+);
+
+const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN;
+const ORIGIN = process.env.ORIGIN;
+const PORT = process.env.PORT;
+const API_SERVICE_URL = process.env.API_URL;
+
+const proxy = createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  cookieDomainRewrite: COOKIE_DOMAIN,
+  selfHandleResponse: true,
+  headers: ORIGIN
+    ? {
+        origin: ORIGIN,
+      }
+    : undefined,
+  onProxyRes: responseInterceptor(
+    async (responseBuffer, proxyRes, req, res) => {
+      const response = responseBuffer.toString("utf8");
+      // modify this if you want to log every response, or comment out this if you don't want to log any response
+      if (res.statusCode >= 400) console.error(response);
+      return response;
+    }
+  ),
+});
+
+app.use(morgan("dev"));
+
+app.use("/", proxy);
+
+app.listen(PORT, 'localhost', () => {
+  console.log(`Starting Proxy at localhost:${PORT}`);
+});
